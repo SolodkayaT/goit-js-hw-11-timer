99066bbce64d19746a8ed01e41675b566ca34e2e
SolodkayaT <tatasladkaya@gmail.com>
2019-12-16 21:14:02 +0200

    add timer class and two timers for example

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const targetDate = new Date(2019, 11, 20, 5, 40).getTime();
 function pad(value) {
   return String(value).padStart(2, '0');
 }
-const CountdownTime = {
-  UpdateTime(day, hour, min, sec) {
-    const refs = {
-      days: document.querySelector('span[data-value="days"]'),
-      hours: document.querySelector('span[data-value="hours"]'),
-      minutes: document.querySelector('span[data-value="mins"]'),
-      seconds: document.querySelector('span[data-value="secs"]'),
-    };
-    refs.days.textContent = day;
-    refs.hours.textContent = hour;
-    refs.minutes.textContent = min;
-    refs.seconds.textContent = sec;
-  },
+
+class CountdownTime {
+  constructor(selector, targetDate) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  updateTime(day, hour, min, sec) {
+    const timerId = this.selector.selector;
+    console.log(timerId);
+    const days = document.querySelector(`${timerId} span[data-value="days"]`);
+    const hours = document.querySelector(`${timerId} span[data-value="hours"]`);
+    const minutes = document.querySelector(
+      `${timerId} span[data-value="mins"]`,
+    );
+    const seconds = document.querySelector(
+      `${timerId} span[data-value="secs"]`,
+    );
+
+    days.textContent = day;
+    hours.textContent = hour;
+    minutes.textContent = min;
+    seconds.textContent = sec;
+  }
 
   start() {
     this.timerId = setInterval(() => {
       const currentTime = Date.now();
 
-      const time = targetDate - currentTime;
+      const time = this.selector.targetDate - currentTime;
 
       const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
       const hours = pad(
@@ -29,9 +39,17 @@ const CountdownTime = {
       const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
       const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-      this.UpdateTime(days, hours, mins, secs);
+      this.updateTime(days, hours, mins, secs);
     }, 1000);
-  },
-};
-
-CountdownTime.start();
+  }
+}
+const timer = new CountdownTime({
+  selector: '#timer-1',
+  targetDate: new Date(2020, 0, 10, 5, 40),
+});
+timer.start();
+const timer2 = new CountdownTime({
+  selector: '#timer-2',
+  targetDate: new Date(2019, 11, 25, 5, 40),
+});
+timer2.start();
